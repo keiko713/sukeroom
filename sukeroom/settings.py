5160142bd8057d73302dfd3067d553e2653db02b
Keiko Oda <keiko713@gmail.com>
2012-08-11 03:53:18 -0700

    Change the path to database

--- sukeroom/settings.py
+++ sukeroom/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sukeroom.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.path.dirname(__file__), '..', 'sukeroom.db').replace('\\','/'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
